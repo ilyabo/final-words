a93001263cb852f8dcffe35a6a8ccaeb38a73475
Ilya Boyandin <ilyabo@gmail.com>
2018-10-26 20:22:35 +0200

    Using emotion instead of glamor

--- wordle-vis/src/App.js
+++ wordle-vis/src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react'
 import { csvParse } from 'd3-dsv'
 import WordCloud from './WordCloud'
-import { insertGlobal, insertRule, css } from 'glamor'
+import { injectGlobal, css } from 'emotion'
 import take from 'lodash/take'
 import isEmpty from 'lodash/isEmpty'
 
 
-insertRule(`
+injectGlobal(`
 @import url('https://fonts.googleapis.com/css?family=Give+You+Glory');
+body {
+  background-color: #cc381e;
+  margin: 0;
+  padding: 0;
+}
+text  {
+ font-family: 'Give You Glory', cursive;
+}
 `)
-// @import url('https://fonts.googleapis.com/css?family=Nanum+Brush+Script');
-  // @import url('https://fonts.googleapis.com/css?family=Homemade+Apple');
-
-insertGlobal('body', {
-  backgroundColor: '#cc381e',
-  margin: 0,
-  padding: 0,
-})
-insertGlobal('text', {
-  fontFamily: "'Give You Glory', cursive",
-  // fontFamily: "'Nanum Brush Script', cursive",
-//  fontFamily: "'Homemade Apple', cursive",
-})
 
 class App extends Component {
 
