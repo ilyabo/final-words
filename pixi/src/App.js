b5a80f63d9164defd48bc2b0714d3adb533a5aec
Ilya Boyandin <ilyabo@gmail.com>
2018-12-24 01:29:11 +0100

    Zoom to selected boxes

--- pixi/src/App.js
+++ pixi/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { csvParseRows } from 'd3-dsv'
+import { csvParse } from 'd3-dsv'
 import StatementsCanvas from './StatementsCanvas'
+import styled from '@emotion/styled'
 
 
 class App extends Component {
@@ -15,9 +16,8 @@ class App extends Component {
     fetch('data/statements.csv')
       .then(response => response.text())
       .then(text => this.setState({
-        statements: csvParseRows(text)
-          .map(([text]) => text)
-          .sort((a, b) => b.length - a.length)
+        statements: csvParse(text)
+          .sort((a, b) => b.statement.length - a.statement.length)
       }))
     this.updateSizes()
   }
@@ -37,17 +37,37 @@ class App extends Component {
     const { width, height, statements } = this.state
     return (
       <div>
-        {width > 0 &&
+        {width > 0 && statements &&
         <StatementsCanvas
           width={width}
           height={height}
           statements={statements}
         />
         }
+        <NavControlsArea>
+          <Button>Prev</Button>
+          <Button>Next</Button>
+        </NavControlsArea>
       </div>
     );
   }
 
 }
+
+const Button = styled.div`
+  background: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  min-width: 
+`
+const NavControlsArea = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  text-align: center;
+  bottom: 20px;
+`
 
 export default App;
--- pixi/src/StatementsCanvas.js
+++ pixi/src/StatementsCanvas.js
@@ -2,19 +2,25 @@ import * as React from 'react'
 import * as PIXI from 'pixi.js'
 import * as d3zoom from 'd3-zoom'
 import * as d3selection from 'd3-selection'
+import { keyframes } from '@emotion/core'
+import styled from '@emotion/styled'
 
-const BOX_WIDTH = 750
 const NUM_COLS = 20
-const BOX_PADDING = 50
-const BOX_SPACING_X = 100
-const BOX_SPACING_Y = 75
+const CARD_WIDTH = 750
+const CARD_PADDING = 50
+const CARD_SPACING_X = 200
+const CARD_SPACING_Y = 200
+const CARD_CHILD__RECT = 'rect'
+const CARD_CHILD__TEXT = 'text'
+const CARD_BG_COLOR = 0xffffff
+const CARD_BG_COLOR__HIGHLIGHTED = 0xe0f0f0
 
 const textStyle = new PIXI.TextStyle({
   fontFamily: "Courier",
   fontSize: 25,
   fill: '#000',
   wordWrap: true,
-  wordWrapWidth: BOX_WIDTH - BOX_PADDING * 2,
+  wordWrapWidth: CARD_WIDTH - CARD_PADDING * 2,
   // stroke: 'steelblue',
   // strokeThickness: 1,
   // dropShadow: true,
@@ -42,17 +48,14 @@ export default class StatementsCanvas extends React.Component {
       view: this.canvasRef.current,
     })
 
-    const nodesContainer = new PIXI.Container()
-    app.stage.addChild(nodesContainer)
-    this.nodesContainer = nodesContainer
-    this.app = app
-    this.updateNodes(this.props.statements)
-
     this.zoom = d3zoom.zoom()
       .on('zoom', this.handleZoom)
 
     this.getZoomTarget().call(this.zoom)
       .on('dblclick.zoom', null)
+
+    this.app = app
+    this.updateNodes(this.props.statements)
   }
 
   getZoomTarget = () => d3selection.select(this.canvasRef.current)
@@ -66,49 +69,116 @@ export default class StatementsCanvas extends React.Component {
     }
   }
 
-  updateNodes(statements) {
-    if (this.nodes) {
-      // remove old nodes
-      this.nodesContainer.removeChildren()
+
+  drawCardRect = (card, color) => {
+    const text = card.getChildByName(CARD_CHILD__TEXT)
+    if (!text) return
+    const height = text.height + CARD_PADDING * 2
+    const width = CARD_WIDTH
+
+    let rect = card.getChildByName(CARD_CHILD__RECT)
+    if (!rect) {
+      rect = new PIXI.Graphics()
+      rect.name = CARD_CHILD__RECT
+      card.addChildAt(rect, 0)
     }
 
-    const nodes = []
-    const columnHeights = new Array(NUM_COLS).fill(0)
+    rect.beginFill(color)
+    rect.drawRoundedRect(0, 0, width, height, CARD_PADDING/2)
+    rect.endFill()
+  }
+
+  handleCardClick = (data) => {
+    const { currentTarget } = data
+    if (currentTarget) {
+      this.getZoomTarget()
+        .transition()
+        .duration(3000)
+        .call(
+          this.zoom.transform,
+          this.zoomTransformToPoint(
+            [currentTarget.x + currentTarget.width/2, currentTarget.y + currentTarget.height / 2],
+            0.75
+          )
+        )
+      // this.drawCardRect(currentTarget, CARD_BG_COLOR__HIGHLIGHTED)
+    }
+  }
+
+  handleCardMouseOver = (data) => {
+    const { currentTarget } = data
+    if (currentTarget) {
+      this.drawCardRect(currentTarget, CARD_BG_COLOR__HIGHLIGHTED)
+    }
+  }
+
+  handleCardMouseOut = (data) => {
+    const { currentTarget } = data
+    if (currentTarget) {
+      this.drawCardRect(currentTarget, CARD_BG_COLOR)
+    }
+  }
+
+  updateNodes(statements) {
+    if (this.nodesContainer) {
+      this.nodesContainer.destroy({ children: true })
+    }
+
     if (statements) {
+      const nodesContainer = new PIXI.Container()
+      const nodes = []
+      const columnHeights = new Array(NUM_COLS).fill(0)
       for (let i = 0; i < statements.length; i++) {
-        const group = new PIXI.Container()
-        const text = new PIXI.Text(statements[i], textStyle)
-
         const col = columnHeights.reduce(((m, d, i) => d < columnHeights[m] ? i : m), 0)
-        group.x = col * (BOX_WIDTH + BOX_SPACING_X)
-        group.y = columnHeights[col]
-        group.addChild(text)
-        columnHeights[col] += text.height + BOX_PADDING * 2 + BOX_SPACING_Y
+        const card = new PIXI.Container()
+        card.interactive = true
+        card.mouseover = this.handleCardMouseOver
+        card.mouseout = this.handleCardMouseOut
+        card.click = this.handleCardClick
 
-        const rect = new PIXI.Graphics()
-        rect.beginFill(0xffffff)
-        rect.drawRoundedRect(-BOX_PADDING, -BOX_PADDING, BOX_WIDTH, text.height + BOX_PADDING * 2, BOX_PADDING/2)
-        rect.endFill()
-        group.addChildAt(rect, 0)
+        const text = new PIXI.Text(statements[i].statement, textStyle)
+        text.name = CARD_CHILD__TEXT
+        text.x = CARD_PADDING
+        text.y = CARD_PADDING
 
-        this.nodesContainer.addChild(group)
-        nodes[i] = group
+        card.x = col * (CARD_WIDTH + CARD_SPACING_X)
+        card.y = columnHeights[col]
+        card.addChild(text)
+        columnHeights[col] += text.height + CARD_PADDING * 2 + CARD_SPACING_Y
+        this.drawCardRect(card, CARD_BG_COLOR)
+
+        nodesContainer.addChild(card)
+        nodes[i] = card
       }
+      this.nodes = nodes
+      this.nodesContainer = nodesContainer
+      this.app.stage.addChild(nodesContainer)
       this.zoomToFit()
     }
-    this.nodes = nodes
   }
+
+
+  zoomTransformToPoint = (point, scale) =>
+    d3zoom.zoomIdentity
+      .translate(this.props.width/2, this.props.height/2)
+      .scale(scale)
+      .translate(-point[0], -point[1])
 
   zoomToFit() {
     const { width, height } = this.props
     const zoomTarget = this.getZoomTarget()
-    this.zoom.translateTo(zoomTarget, this.nodesContainer.width/2, this.nodesContainer.height/2)
-    this.zoom.scaleTo(zoomTarget,
-      Math.min(
-        width / this.nodesContainer.width,
-        height / this.nodesContainer.height,
-      ) * 0.85
-    )
+
+    zoomTarget
+      .call(
+        this.zoom.transform,
+        this.zoomTransformToPoint(
+          [this.nodesContainer.width / 2, this.nodesContainer.height / 2],
+          Math.min(
+            width / this.nodesContainer.width,
+            height / this.nodesContainer.height,
+          ) * 0.7
+        )
+      )
   }
 
   handleZoom = () => {
@@ -117,29 +187,30 @@ export default class StatementsCanvas extends React.Component {
     this.nodesContainer.position = new PIXI.Point(x, y)
   }
 
-  // setup() {
-    // this.app.ticker.add(delta => this.gameLoop(delta))
-  // }
-
-  // gameLoop(delta){
-  //   for (let i = 0; i < this.nodes.length; i++) {
-  //
-  //     //Move the cat 1 pixel
-  //     this.nodes[i].x += Math.random() * 1 - .5
-  //     this.nodes[i].y += Math.random() * 1 - .5
-  //     // nodes[i].rotation += Math.random() * 0.2 - 0.1
-  //   }
-  // }
-
 
   render() {
     const { width, height } = this.props
     return (
-      <canvas
-        width={width}
-        height={height}
-        ref={this.canvasRef}
-      />
+      <FadeIn>
+        <canvas
+          width={width}
+          height={height}
+          ref={this.canvasRef}
+        />
+      </FadeIn>
     )
   }
 }
+
+const fadeIn = keyframes`
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+`
+
+const FadeIn = styled.div`
+  animation: ${fadeIn} 3s ease-in;
+`
